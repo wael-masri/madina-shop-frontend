2823d4924cd89d30c4708cacb99c00de125f0127
wael masri <masri_1997@hotmail.com>
2023-05-23 18:50:57 +0300

    first commit

--- src/Redux/Store/index.js
+++ src/Redux/Store/index.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducers from '../Reducers'
 import reduxThunk from "redux-thunk";
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-const  store = createStore(reducers,enhancer(applyMiddleware(reduxThunk)));
+const enhancer = window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+const  store = createStore(reducers,enhancer?.(applyMiddleware(reduxThunk)));
 
 
 export default store;
